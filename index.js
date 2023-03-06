4c4dd74b94e3f8869c979d0634ac76cdd9f88d87
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-03-05 17:31:45 -0700

    Creates functions using for and while loops

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+const messages = [];
+
+function writeCards(names, value) {
+    for (let i = 0; i < names.length; i++) {
+        messages.push(...[`Thank you, ${names[i]}, for the wonderful ${value} gift!`]);  
+    }
+    return messages;
+}
+
+
+
+function countDown() {
+    let countUp = 10;
+    while (countUp > -1) {
+        console.log(countUp--)
+    }
+
+}
